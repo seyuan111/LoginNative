c01a4496dad8eb3a16bb962fd25594893ca01983
Sean Yuan <ysean54@yahoo.com>
2024-04-22 18:22:21 -0400

    login auth native

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/Login.js
@@ -0,0 +1,85 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <Text style={styles.Title}>Welcome to Immpression</Text>
+        <TextInput placeholder="email" value={email} onChangeText={text => setEmail(text)} style={styles.input} />
+        <TextInput placeholder="password" value={password} onChangeText={text => setPassword(text)} style={styles.input} secureTextEntry />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {}} style={[styles.button, styles.buttonOutline]}>
+            <Text style={styles.buttonOutlineText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    Title: {
+      fontSize: 30,
+      marginBottom: 30,
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        backgroundColor: "lightgray",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 10
+    },
+    buttonContainer: {
+      width: "60%",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 40
+    },
+    button: {
+      backgroundColor: "blue",
+      width: "100%",
+      padding: 10,
+      borderRadius: 10,
+      color: "white"
+    },
+    buttonText: {
+      textAlign: "center",
+      justifyContent: "center",
+      alignItems: 'center',
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 15
+    },
+    buttonOutline: {
+      backgroundColor: "red",
+      marginTop: 10,
+      textAlign: "center",
+      justifyContent: "center",
+      alignItems: 'center'
+    },
+    buttonOutlineText: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 15
+    }
+})
